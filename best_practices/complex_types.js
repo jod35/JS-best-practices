3d80e71a103957186df7f3ffae3bb8e3f548d3c8
jona <jona@jona.com>
2020-04-14 14:16:19 +0300

    Javascript Basics

---
+++ best_practices/complex_types.js
@@ -0,0 +1,11 @@
+/*
+ * These include arrays, function, object
+ */
+const foo=[1,2];
+const bar=foo;
+
+bar[0]=9;
+
+console.log(foo[0],bar[0]);
+
+//returns 9,9
---
+++ best_practices/primitives.js
@@ -0,0 +1,15 @@
+//primitives include
+//string
+//numbers
+//booleans
+//null
+//undefined
+//symbol
+//bigint
+
+const bar=1;
+let foo =0;
+foo=10;
+console.log(bar,foo);
+//returns 1 10
+
