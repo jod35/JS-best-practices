38ad360f01f912f905c71425241f6197bb23522d
jona <jona@jona.com>
2020-04-15 19:54:15 +0300

    regex

---
+++ regex/finding characters with lazy matching.js
@@ -0,0 +1,8 @@
+/*
+there are two types of matching 
+1. greedy matching
+    returns the longest possible part of a string that matches a regex
+2. lazy matching
+    returns the shortest possible part of a string that matches a regex
+
+*/
