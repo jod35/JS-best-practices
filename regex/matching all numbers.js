d50598c3663de672b3a52016fdd8de82b20106e1
jona <jona@jona.com>
2020-04-15 21:57:59 +0300

    regex

---
+++ regex/matching all numbers.js
@@ -0,0 +1,13 @@
+const numVar="2001 A space oddyset";
+
+const myEgex=/[0-9]/g; // this returns [ '2', '0', '0', '1' ]
+
+console.log(numVar.match(myEgex));
+
+
+//this can also be done with \d
+
+const myRegex=/\d/g;
+console.log(numVar.match(myRegex));
+
+// [ '2', '0', '0', '1' ] the /d returns all numeric characters
