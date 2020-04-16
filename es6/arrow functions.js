707ddc8a71e03dbffd155894c105e413ed72743b
jona <jona@jona.com>
2020-04-17 00:18:01 +0300

    es6

---
+++ es6/arrow functions.js
@@ -0,0 +1,19 @@
+//in es5
+// a function is written like this
+
+function add(x,y){
+    console.log(x+y);
+}
+
+console.log(add(3,4)); //7
+
+//using an arrow function
+
+const addnums =(a,b)=>{
+    console.log(a+b);
+}
+
+addnums(3,5); //8
+
+console.log(typeof add); //function
+
