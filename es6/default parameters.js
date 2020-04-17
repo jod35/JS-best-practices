cb14a530145b050968b45233cd86afe701c570bd
jona <jona@jona.com>
2020-04-17 04:41:46 +0300

    es6

--- es6/default parameters.js
+++ es6/default parameters.js
@@ -15,3 +15,25 @@ function add(a,b){ //a,b are params
 }
 
 add(2,3); //2 and 3 are args
+
+
+//evaluating default params
+function addToy(toy,toybox=[]){
+    toybox.push(toy);
+
+    console.log(toybox);
+}
+
+addToy("Car"); //[ 'Car' ]
+
+
+//a function can also take a default value which can be a result of a function call
+function date(d=today()){
+    console.log(d);
+}
+
+function today(){
+    return (new Date()).toDateString("en-US");
+}
+
+date(); //Fri Apr 17 2020
