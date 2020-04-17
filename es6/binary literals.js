4c89d73084f9fecb2cc2812fa4c1c358f318948a
jona <jona@jona.com>
2020-04-17 07:49:36 +0300

    es6

---
+++ es6/binary literals.js
@@ -0,0 +1,7 @@
+// es6 did not offer any form of binary literals
+
+// howerver the parseInt function helps us to convert binary to base 10
+
+const mynum=11101;
+const result=parseInt(mynum,2);
+console.log(result); //29
---
+++ es6/for in loop.js
@@ -0,0 +1,6 @@
+let mynums=[1,2,3,4,5];
+
+for(i in mynums){
+    console.log(i); //it returns the indices of the array
+    
+}
