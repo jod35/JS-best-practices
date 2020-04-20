1f9c72e464bb2d41cb422d5e81d11f5955adc285
jona <jona@jona.com>
2020-04-21 01:50:40 +0300

    data structures

---
+++ data structures/adding items to an array.js
@@ -0,0 +1,11 @@
+let myArray=[1,2,3,4,4,5,6];
+
+//we can use push() to add to the end of an array
+myArray.push(8);
+console.log(myArray);
+//[ 1, 2, 3, 4, 4, 5, 6, 8 ]
+
+//we can use unshift to add to the beginning of an array
+myArray.unshift(0);
+console.log(myArray);
+[ 0, 1, 2, 3, 4, 4, 5, 6, 8 ]
