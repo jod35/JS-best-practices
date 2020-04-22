a2c3f0207bc807749979f747ccf454a0f612a310
jona <jona@jona.com>
2020-04-22 07:17:01 +0300

    OOP

--- OOP/creating instances.js
+++ OOP/creating instances.js
@@ -1,9 +1,11 @@
 //Instances refer to new objects
 
-fucntion Dog(){
+function Dog(){
     this.name="jon";
     this.legs=2;
     this.isBig=true;
 }
 
 let hound= new Dog();
+
+console.log(hound.name);
