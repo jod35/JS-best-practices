c46f30b923e4c99dff0a20b24eb339a87b291f84
jona <jona@jona.com>
2020-04-22 08:08:15 +0300

    OOP

--- OOP/own properties.js
+++ OOP/own properties.js
@@ -1,11 +1,9 @@
 //if we create many objects, then their properties are refered to as own properties
 
 
-
-
 function Bird(name) {
     this.name = name;
-    this.numLegs = 2;
+    this.numLegs = 2; //own properties are defined inside a constructor
   }
   
   let canary = new Bird("Tweety");
